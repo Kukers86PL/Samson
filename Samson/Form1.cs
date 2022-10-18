dd1346f00ca82eff47509bb183af652ebf46386f
Łukasz Grzelak <lukasz.grzelak.86@gmail.com>
2022-10-18 17:08:12 +0200

    Timer added

--- Samson/Form1.cs
+++ Samson/Form1.cs
@@ -7,8 +7,10 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 using System.Windows.Forms;
 using System.Xml.Linq;
+using static System.Net.Mime.MediaTypeNames;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace Samson
@@ -16,16 +18,50 @@ namespace Samson
     public partial class Form1 : Form
     {
         private Form2 form2;
+        private System.Timers.Timer aTimer;
+        private delegate void SafeCallDelegate(Object source, ElapsedEventArgs e);
 
         public Form1()
         {
             InitializeComponent();
+            SetTimer();
         }
 
         private void addToolStripMenuItem_Click(object sender, EventArgs e)
         {
             form2 = new Form2();
             form2.Show();
+        }
+
+        private void OnTimedEvent(Object source, ElapsedEventArgs e)
+        {
+            if (listView1.InvokeRequired)
+            {
+                var d = new SafeCallDelegate(OnTimedEvent);
+                listView1.Invoke(d, new object[] { source, e });
+            }
+            else
+            {
+
+                refreshSettings();
+
+                for (int i = 0; i < listView1.Items.Count; i++)
+                {
+                    string name = listView1.Items[i].SubItems[0].Text;
+                    string priority = listView1.Items[i].SubItems[1].Text;
+                    string affinity = listView1.Items[i].SubItems[2].Text;
+                }
+            }
+        }
+
+        private void SetTimer()
+        {
+            // Create a timer with a two second interval.
+            aTimer = new System.Timers.Timer(5000);
+            // Hook up the Elapsed event for the timer. 
+            aTimer.Elapsed += OnTimedEvent;
+            aTimer.AutoReset = true;
+            aTimer.Enabled = true;
         }
 
         private void refreshSettings()
